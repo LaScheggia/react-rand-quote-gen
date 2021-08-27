c29fcf5b2860fc95db13f97d773301dec8a06402
LaScheggia <lascheggia605@gmail.com>
2021-08-27 13:25:58 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
+import Quote from "./Components/Quote";
 
 function App() {
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null
+  }); 
+
+  const fetchQuote = async () => {
+    return await fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json());
+  }
+
+  useEffect(async() => {
+    setQuote(await fetchQuote());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quote quote={quote}/>
+      <button>
+        Pick New quote
+      </button>
+
     </div>
   );
 }
